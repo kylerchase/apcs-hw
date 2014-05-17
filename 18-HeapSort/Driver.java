c33c587f348b7e0e0f390d50ecedb88432aa1769
Kyler James Chase <kylerchase85@Kylers-MacBook-Air-2.local>
2014-05-17 11:37:42 -0400

    folder 18 has some work from 19

--- 18-HeapSort/Driver.java
+++ 18-HeapSort/Driver.java
@@ -3,8 +3,9 @@ import java.io.*;
 
 public class Driver {
 	public static void main(String[] args) {
-
-	HeapSort h = new HeapSort();
+	//MinHeap h = new MinHeap();
+	//MaxHeap h = new MaxHeap();
+	Heap h = new Heap();
 
 	h.add(13);
 	h.add(42);
@@ -12,16 +13,20 @@ public class Driver {
 	h.add(4);
 	h.add(9);
 	h.add(14);
-	System.out.println(h);
+	//System.out.println(h);
 	
 	System.out.println("///////////////");
 
-	System.out.println(h.remove());
+	//System.out.println(h.remove());
 
-	System.out.println(h);
+	//System.out.println(h);
 
-	h.add(99);
-	System.out.println(Arrays.toString(h.sort()));
+	//h.add(99);
+	//System.out.println(Arrays.toString(h.sort()));
+
+	System.out.println(h.findMedian());
+	System.out.println(h.removeMedian());
+	System.out.println(h.findMedian());
 
 
 
